4f99238a2154d907d9415a5092f183d993555755
Bouchask <boyahya643@gmail.com>
2026-01-23 16:02:40 +0100

    pca

--- preprocissing.py
+++ preprocissing.py
@@ -1,4 +1,5 @@
-from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.preprocessing import StandardScaler,OneHotEncoder,LabelEncoder
+from sklearn.decomposition import PCA
 import numpy as np 
 import pandas as pd 
 class Data :
@@ -7,17 +8,31 @@ class Data :
         self.data = pd.DataFrame(self.data)
         self.scaler = StandardScaler()
         self.oh = OneHotEncoder(handle_unknown="ignore")
+        self.lc = LabelEncoder()
+        self.pca = PCA(n_components=2)
     def clean_data(self,data):
         data = data.dropna()
         data = data.drop_duplicates()
         return data 
     def Scaler_data(self,data,list_feature_number):
         data[list_feature_number] = self.scaler.fit_transform(data[list_feature_number])
+        pca_data = self.pca.fit_transform(data[list_feature_number])
+        return data
+    def pca_data(self , data , list_feature_number ):
+        pca_data = self.pca.fit_transform(data[list_feature_number])
+        pca_data = pd.DataFrame(pca_data,columns = ["pca1","pca2"],index = data.index)
+        data.drop(list_feature_number , axis = 1 , inplace = True)
+        data = pd.concat([data,pca_data],axis = 1)
         return data
     def label_data(self,data,list_feature_object):
-        for i in list_feature_object :
-            data = self.oh.fit_transform(data[i])
-            data.drop(i,axis=1,inplace=True)
+        label_code = self.oh.fit_transform(data[list_feature_object])
+        label_code = pd.DataFrame(label_code.toarray(),columns = self.oh.get_feature_names_out(list_feature_object),index = data.index)
+        data.drop(list_feature_object , axis = 1 , inplace = True)
+        data = pd.concat([data,label_code],axis = 1 )
+        return data 
+    def binaire_feature(self,data , list_feature):
+        for i in list_feature :
+            data[i] = self.lc.fit_transform(data[i])
         return data 
     def dateTo_Second(self,data,list_column_date): #date start - date End
         data[list_column_date[1]] = pd.to_datetime(data[list_column_date[1]])
@@ -31,7 +46,12 @@ class Data :
         data.drop("User ID",axis = 1,inplace=True)
         data = self.dateTo_Second(data,["Charging Start Time","Charging End Time"])
         list_feature_object = data.select_dtypes(include="object").columns
-        data = self.label_data(data,list_feature_object)
-        list_column_number = [name_column for name_column in data.columns if not  (776>= (data[name_column].nunique()) >=2 )]
+        
+        list_column_number = [name_column for name_column in data.columns if (776 < (data[name_column].nunique()))]
+        list_feature_binaire = [name_column for name_column in list_feature_object if ((data[name_column].nunique()) == 2 )]
+        list_feature_ulticlasses =  [name_column for name_column in list_feature_object if (776>= (data[name_column].nunique()) >2 )]
+        data = self.label_data(data,list_feature_ulticlasses)
+        data = self.binaire_feature(data,list_feature_binaire)
         data = self.Scaler_data(data,list_column_number)
-
+        data = self.pca_data(data , list_column_number)
+        return data
